90e660bf238ebea7364e4a57871e5385ffeead42
tomieq <tomieq@wp.pl>
2023-11-16 14:37:45 +0100

    6.5.0 as default dynamic SPM libs

--- Package.swift
+++ Package.swift
@@ -45,16 +45,11 @@ let package = Package(
   platforms: [.iOS(.v9), .macOS(.v10_10), .watchOS(.v3), .tvOS(.v9)],
   products: ([
     [
-      .library(name: "RxSwift", targets: ["RxSwift"]),
-      .library(name: "RxCocoa", targets: ["RxCocoa"]),
-      .library(name: "RxRelay", targets: ["RxRelay"]),
-      .library(name: "RxBlocking", targets: ["RxBlocking"]),
-      .library(name: "RxTest", targets: ["RxTest"]),
-      .library(name: "RxSwift-Dynamic", type: .dynamic, targets: ["RxSwift"]),
-      .library(name: "RxCocoa-Dynamic", type: .dynamic, targets: ["RxCocoa"]),
-      .library(name: "RxRelay-Dynamic", type: .dynamic, targets: ["RxRelay"]),
-      .library(name: "RxBlocking-Dynamic", type: .dynamic, targets: ["RxBlocking"]),
-      .library(name: "RxTest-Dynamic", type: .dynamic, targets: ["RxTest"]),
+      .library(name: "RxSwift", type: .dynamic, targets: ["RxSwift"]),
+      .library(name: "RxCocoa", type: .dynamic, targets: ["RxCocoa"]),
+      .library(name: "RxRelay", type: .dynamic, targets: ["RxRelay"]),
+      .library(name: "RxBlocking", type: .dynamic, targets: ["RxBlocking"]),
+      .library(name: "RxTest", type: .dynamic, targets: ["RxTest"]),
     ],
     Product.allTests()
   ] as [[Product]]).flatMap { $0 },
